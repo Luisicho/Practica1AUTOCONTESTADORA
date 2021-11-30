2b6fbd7f5f899614e02fb5756b24fff897cea161
Enrique Alonso Miramontes Tiznado <enalmiramontesti@ittepic.edu.mx>
2021-11-29 19:58:47 -0700

    aa

--- app/src/main/java/mx/ittepic/tepic/lmhm/ladm_u4_practica1_autocontesta/BaseDatos.kt
+++ app/src/main/java/mx/ittepic/tepic/lmhm/ladm_u4_practica1_autocontesta/BaseDatos.kt
@@ -11,9 +11,7 @@ class BaseDatos(
     version: Int
 ) : SQLiteOpenHelper(context, name, factory, version) {
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("CREATE TABLE ENTRANTES(TELEFONO VARCHAR(200))")
-        db.execSQL("CREATE TABLE LISTANEGRA(ID INTEGER PRIMARY KEY  AUTOINCREMENT,NOMBREN VARCHAR(200),TELEFONON VARCHAR(10))")
-        db.execSQL("CREATE TABLE LISTABLANCA(ID INTEGER PRIMARY KEY  AUTOINCREMENT,NOMBREB VARCHAR(200),TELEFONOB VARCHAR(10))")
+        db.execSQL("CREATE TABLE NUMEROS (TEL VARCHAR(20) PRIMARY KEY)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
